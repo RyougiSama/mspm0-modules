005bd7bb953286115702995ff03d0a06cb8ef56e
ntzxxy <ntzxxy516054@gmail.com>
2025-07-09 23:27:06 +0800

    增加按键调参，pid电机速度调参功能

--- main.c
+++ main.c
@@ -34,10 +34,12 @@
 #include "main.h"
 #include "stdio.h"
 
-uint32_t motor_ms, oled_ms,key_ms;
+uint32_t motor_ms, oled_ms;
 uint16_t test_duty = 10000;
 float val = 0;
 char oled_buffer[20];
+uint8_t key_mode = 0;
+// uint8_t motor_key = 0;
 
 int main(void)
 {
@@ -58,27 +60,33 @@ int main(void)
     OLED_Clear();
 
     Motor_On();
-    pid_init(&g_motorA, DELTA_PID, 10, 5, 3);
-    pid_init(&g_motorB, DELTA_PID, 10, 5, 3);
-    motor_target_set(30,-30);
+    pid_init(&g_motorA, DELTA_PID, 10, 5, 0);
+    // pid_init(&g_motorB, DELTA_PID, 10, 5, 3);
+    motor_target_set(50,0);
 
     while (1)
-        {
-       
+    {
+            Key_PID_MDF();
+            OLED_Task();
+    
+    }
+}
 
-        
-        Key_PID_MDF();
 
-        if (tick_ms - oled_ms >= 500)
+void OLED_Task(void)
+{
+    static uint32_t oled_ms = 0;
+    if(tick_ms - oled_ms >= 500)
+    {
+        oled_ms = tick_ms;
+        OLED_Clear();
+        if(key_mode == 0)
         {
-            oled_ms = tick_ms;
-            OLED_Clear();
             OLED_ShowString(4, 0, (uint8_t *)"En1:", 16);
-            OLED_ShowString(4, 2, (uint8_t *)"En2:", 16);
-            
+           
             OLED_ShowNum(40, 0, (uint32_t)g_motorA.now, 3, 16);
-            OLED_ShowNum(40, 2, (uint32_t)g_motorB.now, 3, 16);
-            // --- 显示I值 ---
+
+            // --- 显示P值 ---
             sprintf((char *)oled_buffer, "AP:%-4.2f", g_motorA.p); 
             OLED_ShowString(0, 4, (uint8_t*)oled_buffer, 16);
 
@@ -88,8 +96,26 @@ int main(void)
 
             // --- 显示D值 ---
             sprintf((char *)oled_buffer, "AD:%-4.2f", g_motorA.d);
-            OLED_ShowString(65, 6, (uint8_t*)oled_buffer, 16);
-        }
+            OLED_ShowString(0, 6, (uint8_t*)oled_buffer, 16);
 
+        }
+        else if(key_mode == 1)
+        {
+            OLED_ShowString(4, 0, (uint8_t *)"En2:", 16);
+            OLED_ShowNum(40, 2, (uint32_t)g_motorB.now, 3, 16);
+
+            // --- 显示P值 ---
+            sprintf((char *)oled_buffer, "BP:%-4.2f", g_motorB.p); 
+            OLED_ShowString(0, 4, (uint8_t*)oled_buffer, 16);
+
+            // --- 显示I值 ---
+            sprintf((char *)oled_buffer, "BI:%-4.2f", g_motorB.i);
+            OLED_ShowString(65, 4, (uint8_t*)oled_buffer, 16);
+
+            // --- 显示D值 ---
+            sprintf((char *)oled_buffer, "BD:%-4.2f", g_motorB.d);
+            OLED_ShowString(0, 6, (uint8_t*)oled_buffer, 16);
+        }
+            
     }
 }
